1a08481502b9d86ead81d90da8b9b075d91728fb
Jeremy Hamm <jh@jeremyhamm.dev>
2020-08-23 16:18:24 -0700

    Add current date

--- time-of-use.js
+++ time-of-use.js
@@ -1,8 +1,44 @@
+/**
+ * Translate day of week number to name
+ * 
+ * @param {Number} - day of week 0-6
+ * 
+ * @return {String} - day of the week name
+*/
+const getDayofWeek = (dow) => {
+  switch (dow) {
+    case 0:
+      return 'Sunday';
+    case 1:
+      return 'Monday';
+    case 2:
+      return 'Tuesday';
+    case 3:
+      return 'Wednesday';
+    case 4:
+      return 'Thursday';
+    case 5:
+      return 'Friday';
+    case 6:
+      return 'Saturday';
+  }
+}
+
+/**
+* Get current date (mm/dd/yyyy)
+* 
+* @return {String}
+*/
+const getCurrentDate = () => {
+  const now = new Date();
+  return `${getDayofWeek(now.getDay())} ${now.getMonth() + 1}/${now.getDate()}/${now.getFullYear()}`
+}
+
 class ContentCardExample extends HTMLElement {
   set hass(hass) {
     if (!this.content) {
       const card = document.createElement('ha-card');
-      card.header = 'SDGE Current Tier';
+      card.header = 'SDGE Time of Use';
       this.content = document.createElement('div');
       this.content.style.padding = '0 16px 16px';
       card.appendChild(this.content);
@@ -12,17 +48,15 @@ class ContentCardExample extends HTMLElement {
     const entityId = this.config.entity;
     const state = hass.states[entityId];
     const stateStr = state ? state.state : 'unavailable';
-    //const date = getDatetime();
 
     this.content.innerHTML = `
-      TEST
+      Today is ${getCurrentDate()}
+      <br><br>
+      <img src="https://cdn.shopify.com/s/files/1/1465/8230/files/whitney-mnt_large.jpg?v=1475605534">
     `;
   }
 
   setConfig(config) {
-    if (!config.entity) {
-      throw new Error('You need to define an entity');
-    }
     this.config = config;
   }
 
@@ -31,11 +65,6 @@ class ContentCardExample extends HTMLElement {
   getCardSize() {
     return 3;
   }
-
-  // getDatetime() {
-  //   const now = Date();
-  //   return now;
-  // }
 }
 
 customElements.define('time-of-use', ContentCardExample);
