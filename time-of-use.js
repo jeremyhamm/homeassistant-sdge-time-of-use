ff95e935eb828b41c90673c6f1c86de225e8c7fc
Jeremy Hamm <jeremyhamm@users.noreply.github.com>
2020-10-24 20:30:08 -0700

    Update time-of-use.js

--- time-of-use.js
+++ time-of-use.js
@@ -37,42 +37,46 @@ const getCurrentDate = (date) => {
  * SDGE Tiers by day and hour of day
 */
 const tiers = {
-  'weekday': {
-    'super-off-peak': [0,1,2,3,4,5],
-    'off-peak': [6,7,8,9,10,11,12,13,14,15,21,22,23],
-    'on-peak': [16,17,18,19,20]
-  },
-  'weekend': {
-    'super-off-peak': [0,1,2,3,4,5,6,7,8,9,10,11,12,13],
-    'off-peak': [14,15,21,22,23],
-    'on-peak': [16,17,18,19,20]
+  'TOU-DR1': {
+    'weekday': {
+      'super-off-peak': [0,1,2,3,4,5],
+      'off-peak': [6,7,8,9,10,11,12,13,14,15,21,22,23],
+      'on-peak': [16,17,18,19,20]
+    },
+    'weekend': {
+      'super-off-peak': [0,1,2,3,4,5,6,7,8,9,10,11,12,13],
+      'off-peak': [14,15,21,22,23],
+      'on-peak': [16,17,18,19,20]
+    }
   }
 }
 
 /**
  * Calculate current SDGE time of use
  * 
- * @param {Number} - current time
+ * @param {String} plan - sdge plan
+ * @param {Number} time - hour of day
+ * @param {Number} day - day of the week
  * 
  * @return {String} - SDGE time of use
 */
-const calculateTimeofUse = (time, day) => {
+const calculateTimeofUse = (plan, time, day) => {
   if ( day === 0 || day === 6 ) {
-    for (const tier in tiers.weekend) {
-      if ( tiers.weekend[tier].includes(time) ) {
+    for (const tier in tiers[plan].weekend) {
+      if ( tiers[plan].weekend[tier].includes(time) ) {
         return tier;
       }
-    }   
+    }
   } else {
-    for (const tier in tiers.weekday) {
-      if ( tiers.weekday[tier].includes(time) ) {
+    for (const tier in tiers[plan].weekday) {
+      if ( tiers[plan].weekday[tier].includes(time) ) {
         return tier;
       }
     }
   }
 }
 
-class ContentCardExample extends HTMLElement {
+class TimeOfUse extends HTMLElement {
   set hass(hass) {
     if (!this.content) {
       const card = document.createElement('ha-card');
@@ -84,10 +88,13 @@ class ContentCardExample extends HTMLElement {
       card.appendChild(this.content);
       this.appendChild(card);
       
+      // Energy plan
+      const energyPlan = this.config.plan;
+      
       // Time of use
       const now = new Date();
       const currentDate = getCurrentDate(now);
-      const timeOfUse = calculateTimeofUse(now.getHours(), now.getDay());
+      const timeOfUse = calculateTimeofUse(energyPlan, now.getHours(), now.getDay());
         
       if ( timeOfUse === 'super-off-peak' ) {
         card.style.background = '#b1bf4a';
@@ -108,6 +115,9 @@ class ContentCardExample extends HTMLElement {
   }
 
   setConfig(config) {
+    if (!config.plan) {
+      throw new Error('You need to define a plan');
+    }
     this.config = config;
   }
 
@@ -118,4 +128,4 @@ class ContentCardExample extends HTMLElement {
   }
 }
 
-customElements.define('time-of-use', ContentCardExample);
+customElements.define('time-of-use', TimeOfUse);
